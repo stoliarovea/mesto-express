89aafc128ca439aa9732fadd116e1d6960fd5382
stoliarovea <stoliarovea@gmail.com>
2023-06-03 22:09:52 +0300

    03.06.2023-7

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const users = require('./routes/users');
 const cards = require('./routes/cards');
 
-const { PORT = 3000 } = process.env;
+const { PORT } = process.env;
 
 const app = express();
 
