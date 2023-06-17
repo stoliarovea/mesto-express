e5bc8e433618b3ab5dc53d0aca2be41aa5a2fda2
stoliarovea <stoliarovea@gmail.com>
2023-06-17 12:05:08 +0300

    17.06.2023-1

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
 const users = require('./routes/users');
 const cards = require('./routes/cards');
 const { createUser, login } = require('./controllers/users');
@@ -21,8 +23,22 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().url(),
+  }),
+}), createUser);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
 
 app.use(auth);
 
@@ -33,6 +49,8 @@ app.use((req, res) => {
   res.status(404).send({ message: 'Page not found' });
 });
 
+app.use(errors());
+
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res
@@ -40,7 +58,7 @@ app.use((err, req, res, next) => {
     .send({
       message: statusCode === 500
         ? 'Internal server error'
-        : message
+        : message,
     });
 });
 
