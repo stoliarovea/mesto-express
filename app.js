a55e967ab000fa6e1e3556f42a719c09fed27927
stoliarovea <stoliarovea@gmail.com>
2023-06-18 23:54:39 +0300

    18.06.2023-1

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.post('/signup', celebrate({
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().uri(),
   }),
 }), createUser);
 
@@ -40,7 +40,7 @@ app.post('/signin', celebrate({
   }),
 }), login);
 
-//app.use(auth);
+app.use(auth);
 
 app.use(users);
 app.use(cards);
